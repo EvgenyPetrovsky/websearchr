fa2b62da0be97cbee7c96581bdee911bed458042
EvgenyPetrovsky <fonpetroff@mail.ru>
2019-05-22 18:07:06 +0200

    add duckduckgo search

--- R/duckduckgo.R
+++ R/duckduckgo.R
@@ -1,18 +1,48 @@
+#' Initiate duckduckgo search
+#'
+#' @export
+#'
+init_duck_search <- function() {
+  fun <- function(query, n, page) {duck_search(q = query)}
 
-duck_url <- "https://api.duckduckgo.com"
-name_of_app <- "websearchr"
-
-duck_header <- function() {
-  httr::add_headers(httr::accept_json())
+  return(fun)
 }
 
-duck_call <- function(
-  query
+#' DuckDuckGo low-lewel search function
+#'
+#'
+duck_search <- function(
+  q
 ) {
-  httr::GET(
-    url = duck_url,
-    httr::accept_json(),
-    query = list(
-      # no_html - to remove html markup; t - to provide name of application
-      no_html = 1, t = name_of_app))
+  duck_url <- "https://api.duckduckgo.com"
+  name_of_app <- "websearchr"
+
+  r <-
+    httr::GET(
+      url = duck_url,
+      query = list(
+        # no_html - to remove html markup; t - to provide name of application
+        q = q,
+        no_html = 1, format = "json", pretty = 1, no_redirect = 1,
+        t = name_of_app
+      )
+    ) %>%
+    httr::stop_for_status()
+
+  content <- r %>%
+    httr::content(as = "parsed", type = "application/json")
+
+  related_topics <- content$RelatedTopics
+
+  related_topics %>%
+    Map(
+      f = function(x) {
+        data.frame(
+          id = x$Result, url = x$FirstURL, title = x$Text,
+          stringsAsFactors = F
+        )
+      }
+    ) %>%
+    Reduce(f = rbind)
+
 }
